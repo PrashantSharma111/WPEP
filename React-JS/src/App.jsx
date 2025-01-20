5447fc5aae59fe48430eca5bbecbc92065941e63
PrashantSharma111 <prashantkumarsharma2005@gmail.com>
2025-01-20 20:34:36 +0530

    Initial Commit

---
+++ React-JS/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ClassHead from "./Day-06/ClassHead";
+
+function App() {
+  return (
+    <>
+      <ClassHead />
+    </>
+  );
+}
+
+export default App;
---
+++ React-JS/src/Day-06/ClassHead.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+
+class ClassHead extends React.Component {
+  render() {
+    return <div>
+        <p className="para">I am header Component!</p>
+    </div>;
+  }
+}
+
+export default ClassHead;
