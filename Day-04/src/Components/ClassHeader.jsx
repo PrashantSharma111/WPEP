14f5a11e69cb566f38f2b06c4ef1532af5fd52b7
PrashantSharma111 <prashantkumarsharma2005@gmail.com>
2025-02-05 11:47:13 +0530

    Day 04

---
+++ Day-04/src/Components/ClassHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'style.css'
+
+function ClassHeader() {
+  return (
+    <div>
+      <p className='para'>I am the header component!</p>
+    </div>
+  )
+}
+
+export default ClassHeader;
